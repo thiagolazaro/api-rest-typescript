1827c8318288c59f449e3ca5dc3764a31898934b
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-10-23 23:08:58 -0300

    Criando configuração de migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -11,4 +11,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  entities: [`src/entities/*.{ts,js}`],
+  migrations: [`src/database/migrations/*.{ts,js}`],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { AppDataSource } from "./data-source";
+import { AppDataSource } from "./database/data-source";
 
 AppDataSource.initialize().then(() => {
   const app = express();
